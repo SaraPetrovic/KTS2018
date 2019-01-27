2a1bccea644bb5643e438919c91001e1aaf61001
lakovicj <31940189+lakovicj@users.noreply.github.com>
2019-01-27 23:45:13 +0100

    PriceList FRONT !!!!!

--- transport/src/main/java/ftn/kts/transport/controllers/PriceListController.java
+++ transport/src/main/java/ftn/kts/transport/controllers/PriceListController.java
@@ -6,6 +6,7 @@ import javax.ws.rs.Produces;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -30,6 +31,7 @@ public class PriceListController {
 	@PostMapping
 	@Produces("application/json")
 	@Consumes("application/json")
+	@CrossOrigin( origins = "http://localhost:4200")
 	public ResponseEntity<PriceList> addNewPriceList(@RequestBody PriceListDTO priceListDTO) {
 		PriceList newPriceList = dtoConverter.convertDTOtoPriceList(priceListDTO);
 		PriceList ret = priceListService.addPriceList(newPriceList);
