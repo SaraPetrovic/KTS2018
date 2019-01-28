95027df7a7bfeb550acf89d395732d01eb77845b
Balenko <marko.balenovic23@gmail.com>
2019-01-28 16:56:14 +0100

    neki testovi

---
+++ transport/src/test/java/ftn/kts/transport/e2e/tests/LoginTest.java
@@ -0,0 +1,27 @@
+package ftn.kts.transport.e2e.tests;
+
+
+import ftn.kts.transport.e2e.MainPage;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginTest {
+
+    private WebDriver browser;
+
+    private MainPage mainPage;
+
+    @Before
+    public void setUpSelenium(){
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Balenko\\Desktop\\chromedriver.exe");
+
+        this.browser = new ChromeDriver();
+        this.browser.manage().window().maximize();
+        this.browser.navigate().to("http://localhost:4200");
+    }
+
+    @Test
+    public void test(){}
+}
