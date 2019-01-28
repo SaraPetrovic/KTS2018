b3e9d1750dd97b93e421998bd29636a2fdb22072
Sara <sarapetrovic03@gmail.com>
2019-01-28 18:54:02 +0100

    Potreban pull

--- transport/src/main/java/ftn/kts/transport/e2e/UserEditProfilePage.java
+++ transport/src/main/java/ftn/kts/transport/e2e/UserEditProfilePage.java
@@ -3,6 +3,8 @@ package ftn.kts.transport.e2e;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class UserEditProfilePage {
 	
@@ -26,15 +28,26 @@ public class UserEditProfilePage {
 	@FindBy(xpath = "//span[contains(text(),'Save')]")
 	private WebElement saveButton;
 
+	@FindBy(xpath = "//a[contains(text(),'Sara')]")
+	private WebElement profileLink;
+	
 	
 	public UserEditProfilePage(WebDriver driver) {
 		this.driver = driver;
 	}
 
+	public void ensureIsDisplayed(){
+        (new WebDriverWait(driver, 10)).until(ExpectedConditions.visibilityOf(profileLink));
+    }
+	
 	public WebElement getFirstName() {
 		return firstName;
 	}
 
+	public WebElement getProfileLink() {
+		return profileLink;
+	}
+	
 	public void setFirstName(String value) {
 		WebElement el = getFirstName();
 		el.clear();
@@ -85,6 +98,14 @@ public class UserEditProfilePage {
 		return saveButton;
 	}
 	
+	public boolean isButtonEnabled() {
+        try {
+            (new WebDriverWait(driver, 3)).until(ExpectedConditions.elementToBeClickable(saveButton));
+            return true;
+        }catch(Exception e){
+            return false;
+        }
+    }
 	
 	
 }
